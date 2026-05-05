5327819de9dd2d2c1bb1b70f71ee7dc008ed2fe6
Mohamed Rifi <mohaa.rifii@gmail.com>
2026-05-05 18:18:41 +0200

    Fix Azure SQL encryption forced

--- src/config/db.js
+++ src/config/db.js
@@ -5,8 +5,8 @@ const dbConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   server: process.env.DB_SERVER,
-  port: Number(process.env.DB_PORT || 1433),
   database: process.env.DB_DATABASE,
+  port: parseInt(process.env.DB_PORT, 10),
   options: {
     encrypt: true,
     trustServerCertificate: false
